7725d110a34411feecb20d8c228738c2bb804364
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2021-05-19 14:51:49 +0200

    Can not reproduce it

--- ReactiveUiTwoWayBindingWithChangesIssue/MainWindow.xaml.cs
+++ ReactiveUiTwoWayBindingWithChangesIssue/MainWindow.xaml.cs
@@ -1,13 +1,73 @@
-﻿namespace ReactiveUiTwoWayBindingWithChangesIssue
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Windows.Documents;
+using DynamicData.Binding;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace ReactiveUiTwoWayBindingWithChangesIssue
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow
+    public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
     {
         public MainWindow()
         {
+            ViewModel = new MainWindowViewModel();
             InitializeComponent();
+            this.WhenActivated(d =>
+            {
+                var regions = new List<string> {"US", "EG", "UK"};
+                this.Regions.ItemsSource = regions;
+                var nRegions = new List<int> { 16, 55, 86};
+                
+                this.Bind(ViewModel, vm => vm.DatabaseServices.DatabaseSettings.RegionCode, v => v.Regions.SelectedItem,
+                    v => v < 0 ? "" : regions[v], v => regions.IndexOf((string)v)).DisposeWith(d);
+                this.ViewModel.WhenPropertyChanged(vm => vm.DatabaseServices.DatabaseSettings.RegionCode, false)
+                    .Subscribe(_ =>
+                    {
+                        
+                    }).DisposeWith(d);
+            });
+
+        }
+    }
+
+    public class MainWindowViewModel : ReactiveObject
+    {
+        [Reactive] public DatabaseServices DatabaseServices { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            DatabaseServices = new DatabaseServices();
+        }
+    }
+
+    public class DatabaseServices : ReactiveObject
+    {
+        [Reactive] public DatabaseSettings DatabaseSettings { get; private set; }
+
+        public DatabaseServices()
+        {
+            DatabaseSettings = new DatabaseSettings();
+        }
+    }
+
+    public class DatabaseSettings : ReactiveObject
+    {
+        [Reactive] public int RegionCode { get; set; }
+
+        public DatabaseSettings()
+        {
+            Observable.Timer(TimeSpan.FromSeconds(2))
+                .ObserveOnDispatcher()
+                .Subscribe(_ =>
+            {
+                RegionCode = 2;
+            });
         }
     }
 }

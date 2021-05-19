fe9c1a26496c31ee00bb9ac4704fd74cf5c21fc1
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2021-05-19 14:21:59 +0200

    Initial commit

---
+++ ReactiveUiTwoWayBindingWithChangesIssue/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ReactiveUiTwoWayBindingWithChangesIssue
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ ReactiveUiTwoWayBindingWithChangesIssue/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ReactiveUiTwoWayBindingWithChangesIssue
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
